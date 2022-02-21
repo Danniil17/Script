54e9320e73ed0da0396aa4fd60bb99ead13072d1
Danniil17 <suvorovit04@local>
2022-02-21 15:31:09 +0300

    2nd version

--- Tablichka2.sql
+++ Tablichka2.sql
@@ -11,15 +11,21 @@ create table if not exists Albums (
 	
 );
  
-create table if not exists Tracks (
-	id serial, 
+create table if not exists Tracks (	
+	id serial primary key, 
 	TrackName varchar(80),
 	TrackDuration integer,
-	AlbumId integer,
-	ArtistId integer
+	AlbumId integer not null references Albums
+	
 );
 
 create table if not exists Genres (
 	id serial primary key,
 	GenreName varchar(60)
+);
+
+create table if not exists ArtistGenres (
+		artist_id integer references Artists(id),
+		genres_id integer references Genres(id),
+		constraint pk primary key (genres_id, artist_id)
 );
